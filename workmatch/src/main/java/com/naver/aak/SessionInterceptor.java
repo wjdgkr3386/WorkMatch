4757c7951935028ca4958973b15417cd74799ff9
Jeonghak Kim <wjdgkr3386@naver.com>
2024-07-18 00:21:56 +0900

    로그인 추가

--- workmatch/src/main/java/com/naver/aak/SessionInterceptor.java
+++ workmatch/src/main/java/com/naver/aak/SessionInterceptor.java
@@ -4,7 +4,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 
@@ -16,12 +15,6 @@ public class SessionInterceptor   implements HandlerInterceptor  {
 			, HttpServletResponse response 
 			, Object handler
 	) throws Exception {
-
-        if (handler instanceof HandlerMethod) {
-            HandlerMethod handlerMethod = (HandlerMethod) handler;
-            String className = handlerMethod.getBeanType().getName();
-            String methodName = handlerMethod.getMethod().getName();
-        }
         
 		HttpSession session = request.getSession();   
 		String mid = (String)session.getAttribute("mid");	
